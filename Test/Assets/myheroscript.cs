25c88cf5bb958c357d6b86f803bdec3278089d2c
t00237814 <sami.abdrhman2@students.ittralee.ie>
2024-10-03 10:52:46 +0100

    Signed-off-by: t00237814 <sami.abdrhman2@students.ittralee.ie>

--- Test/Assets/myheroscript.cs
+++ Test/Assets/myheroscript.cs
@@ -4,19 +4,54 @@ using UnityEngine;
 
 public class myheroscript : MonoBehaviour
 {
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        animator.SetBool("is running", false);
+
         if (Input.GetKey(KeyCode.W))
+       {
+
             transform.position += transform.forward * Time.deltaTime;
+            animator.SetBool("is running", true);
+
+        }
 
         if (Input.GetKey(KeyCode.A))
-            transform.Rotate(Vector3.up, 30 * Time.deltaTime);
+            transform.Rotate(Vector3.up, 120 * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.S))
+            transform.position -= transform.forward * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(Vector3.down,120*Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.Q))
+            transform.position += Vector3.left * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.E))
+            transform.position += Vector3.right * Time.deltaTime;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        print(collision.gameObject);
+        
+       
+        footballscript myFootball = collision.gameObject.GetComponent<footballscript>();
+        if ( myFootball != null) 
+        {
+            myFootball.Kick();
+        }
+
+
+
     }
 }
